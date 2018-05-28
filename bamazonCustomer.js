1c2a9242ea24458e4afb00524c5ac5bbd0fb3578
Linh Tong <linhthitong@gmail.com>
2018-05-27 23:40:43 -0700

    add bamazonCustomer.js, products-schema and products

---
+++ bamazonCustomer.js
@@ -0,0 +1,35 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "",
+    database: "bamazon_DB"
+
+});
+
+//connect to the mysql server and sql database
+connection.connect(function(err) {
+    if(err) throw err;
+//run the start function after the connection is made to prompt  the user
+    start();
+
+});
+
+// function which prompts the user for what action they should take
+function start() {
+    inquirer
+        .prompt({
+            name: "purchaseOrQuit",
+            type: "rawlist",
+            message: "What is the item ID you would like to purchase?"
+
+        })
+
+}
